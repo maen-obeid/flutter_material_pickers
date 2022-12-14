d34efa3def37b2c230d935a3307e8060f8355502
Maen Obeid <maen.obeid@mobisoft-me.com>
2022-12-14 11:39:30 +0300

    fixing the height

--- lib/pickers/scroll_picker.dart
+++ lib/pickers/scroll_picker.dart
@@ -91,7 +91,7 @@ class _ScrollPickerState<T> extends State<ScrollPicker<T>> {
                 controller: scrollController,
                 itemExtent: itemHeight,
                 onSelectedItemChanged: _onSelectedItemChanged,
-                physics: FixedExtentScrollPhysics(),
+                physics: AlwaysScrollableScrollPhysics(),
               ),
             ),
             Center(child: widget.showDivider ? Divider() : Container()),
